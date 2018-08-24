8f0003c75f9b49a137f490c9e8f836ceff778fd5
Wonho ha <haho013039@gmail.com>
2018-08-24 09:44:52 +0900

    Add: Test

--- Server/test/unit_test.py
+++ Server/test/unit_test.py
@@ -1,2 +1,16 @@
+import selenium as sl
+from selenium import webdriver
+
+
 def test():
     assert 1
+
+
+def test_server():
+    driver_Path ="./chromedriver.exe"
+    driver = webdriver.Firefox()
+
+    driver.get("http://localhost:5000/")
+
+    assert 'Hello World!' == driver.find_element_by_xpath('/html/body/pre').text
+    
