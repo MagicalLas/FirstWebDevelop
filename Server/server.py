08995afccea715b634046fdcef074a83ba6c663b
Unperknown <wnsdud105@gmail.com>
2018-08-29 22:14:41 +0900

    Integrated server py file

--- Server/server.py
+++ Server/server.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('home.html')
 
-@app.route('/project')
-def index_():
-    return 'This is FWD Project'
+@app.route('/user/<name>')
+def index_when_signed_in(name):
+    return render_template('home.html', name=name)
+
+@app.route('/login/')
+def login():
+    return render_template('sign_in.html')
+
+@app.route('/info/')
+def info():
+    return render_template('info.html')
+
+@app.route('/gallery/')
+def gall():
+    return render_template('gallery.html')
+
+@app.route('/stat/')
+def stat():
+    return render_template('stat.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
