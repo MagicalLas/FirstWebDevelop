ac07a3fe4a4f73acc0509a50505ffbab9c5e3973
Wonho ha <haho013039@gmail.com>
2018-09-06 20:44:30 +0900

    간단한 이름 변화

--- Server/server.py
+++ Server/server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main_page.html')
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
