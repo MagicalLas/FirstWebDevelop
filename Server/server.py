461c65175bb4ce2fe86c7cd4bfd5953ec08096eb
Wonho ha <haho013039@gmail.com>
2018-09-06 20:44:08 +0900

    Delete
    쓸모 없는 기능을 싹 정리 했습니다. 하나의 기능씩 구현해 나가도록 하겠습니다.

--- Server/server.py
+++ Server/server.py
@@ -1,43 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    return render_template('home.html')
-
-@app.route('/user/<name>')
-def index_when_signed_in(name):
-    return render_template('home.html', name=name)
-
-@app.route('/login/')
-def login():
-    return render_template('sign_in.html')
-
-@app.route('/info/')
-def info():
-    return render_template('info.html')
-
-@app.route('/gallery/')
-def gall():
-    return render_template('gallery.html')
-
-@app.route('/stat/')
-def stat():
-    return render_template('stat.html')
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-@app.route('/main')
-def main_page():
-    return '<h1>MAIN PAGES</h1>'
-
+    return render_template('main_page.html')
 
 if __name__ == '__main__':
     app.run()
