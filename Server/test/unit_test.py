21d9d2f3fef5f508c42021854ae0b826349389cf
Wonho ha <haho013039@gmail.com>
2018-09-06 20:19:33 +0900

    테스트 수정

--- Server/test/unit_test.py
+++ Server/test/unit_test.py
@@ -10,4 +10,3 @@ def test_server():
     driver = webdriver.Chrome()
     driver.get('http://localhost:5000')
 
-    assert driver.find_element_by_xpath('/html/body/h1').text == "Welcome to our site!"
