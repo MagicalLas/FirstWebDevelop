10b3715421351aab9379b01403d8c7d6ecf3d56b
Wonho ha <haho013039@gmail.com>
2018-09-12 11:26:58 +0900

    #18 필요없는 파일 제거

--- server.py
+++ server.py
@@ -36,11 +36,11 @@ def stat():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('error.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('error.html'), 500
 
 if __name__ == '__main__':
     app.run()
